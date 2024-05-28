2855ef77bb11eb35b9b42de3ec7fc8b9c18a1c7d
Weilei uses root on qlab-gpu-8-1 <weileizeng@zhejianglan.com>
2024-05-28 17:09:34 +0800

    print plotted files

--- all-plots.py
+++ all-plots.py
@@ -8,10 +8,14 @@ filelist = glob.glob(f'{result_folder}/*loss.pt')
 
 import time
 now = time.time()
-delta = 60 # seconds
+delta = 600 # seconds
+plotted=[]
 for f in filelist:
     #print(f)
     t=os.path.getmtime(f)
     if (now - t) < delta:        
         #print(t,f)
         os.system(f'make f={f} plot')
+        plotted.append(f)
+
+print('finish plotting for:\n','\n'.join(plotted))
