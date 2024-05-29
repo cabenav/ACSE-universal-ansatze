d619a675c9834aa3d829311b5b705ea7e3806997
Weilei uses root on qlab-gpu-8-1 <weileizeng@zhejianglan.com>
2024-05-29 14:51:41 +0800

    format print out; add skipped files

--- all-plots.py
+++ all-plots.py
@@ -10,6 +10,7 @@ import time
 #now = time.time()
 #delta = 600 # seconds
 plotted=[]
+skipped=[]
 for f in filelist:
     #print(f)
     t=os.path.getmtime(f)
@@ -21,11 +22,18 @@ for f in filelist:
         t_fig = t-1 #plot for new data file
     if t > t_fig:
         print(f'data file updated for {filename_fig}. replotting...')
-#        continue
-#    if False or (now - t) < delta:        
+        #        continue
+        #    if False or (now - t) < delta:        
         #print(t,f)
         os.system(f'make f={f} plot')
         plotted.append(f)
+    else:
+        print(f'skip {f}')
+        skipped.append(f)
 
+print('#'*120)
+print('skipped')
+print('\n'.join(skipped))
 print('finish plotting for:')
 print('\n'.join(plotted))
+print('#'*120)
