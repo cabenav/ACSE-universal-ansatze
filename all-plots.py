6e3c8fe78d6f0c8fb3fba71119c7026f1e16aeb2
Weilei uses root on qlab-gpu-8-1 <weileizeng@zhejianglan.com>
2024-05-29 14:31:39 +0800

    plot for updated data file

--- all-plots.py
+++ all-plots.py
@@ -1,21 +1,28 @@
 # plot all loss file in given folder
 import os
 import glob
-
+fig_folder='fig'
 result_folder='checkpoints'
 filelist = glob.glob(f'{result_folder}/*loss.pt')
 #print(filelist)
 
 import time
-now = time.time()
-delta = 600 # seconds
+#now = time.time()
+#delta = 600 # seconds
 plotted=[]
 for f in filelist:
     #print(f)
     t=os.path.getmtime(f)
-    if (now - t) < delta:        
+    filename=f.split('/')[-1]
+    filename_fig=f'{fig_folder}/{filename}.pdf'
+    t_fig = os.path.getmtime(filename_fig)
+    if t > t_fig:
+        print(f'data file updated for {filename_fig}. replotting...')
+#        continue
+#    if False or (now - t) < delta:        
         #print(t,f)
         os.system(f'make f={f} plot')
         plotted.append(f)
 
-print('finish plotting for:\n','\n'.join(plotted))
+print('finish plotting for:')
+print('\n'.join(plotted))
