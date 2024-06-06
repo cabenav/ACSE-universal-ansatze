c87b4de795684b2beacc8b93a107e1e3c53d8258
Weilei uses root on qlab-gpu-8-1 <weileizeng@zhejianglan.com>
2024-06-06 16:04:54 +0800

    enable replotting all; plot in parallel

--- all-plots.py
+++ all-plots.py
@@ -6,9 +6,18 @@ result_folder='checkpoints'
 filelist = glob.glob(f'{result_folder}/*loss.pt')
 #print(filelist)
 
+
+from threading import Thread
+
+def plot(f): #f is the name of the data file to be plotted
+    os.system(f'make f={f} plot')
+    
+PLOT_ALL=False # True for plotting all; False for new data only
+
 import time
 #now = time.time()
 #delta = 600 # seconds
+thread_pool=[]
 plotted=[]
 skipped=[]
 for f in filelist:
@@ -19,17 +28,23 @@ for f in filelist:
     if os.path.exists(filename_fig):
         t_fig = os.path.getmtime(filename_fig)
     else:
-        t_fig = t-1 #plot for new data file
-    if t > t_fig:
+        t_fig = t-1 #plot for newly-created data file
+    if PLOT_ALL or t > t_fig:
         print(f'data file updated for {filename_fig}. replotting...')
         #        continue
         #    if False or (now - t) < delta:        
         #print(t,f)
-        os.system(f'make f={f} plot')
+        thread = Thread(target = plot, args = (f, ))
+        thread_pool.append(thread)
+        thread.start()        
+        #os.system(f'make f={f} plot')
         plotted.append(f)
     else:
         print(f'skip {f}')
         skipped.append(f)
+
+for thread in thread_pool:
+    thread.join()
 
 print('#'*120)
 print('skipped')
