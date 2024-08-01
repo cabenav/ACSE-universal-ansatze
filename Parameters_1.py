7e5543f6b7534e1dc80862b421acb3c78524449e
Carlos L. Benavides-Riveros <58853939+cabenav@users.noreply.github.com>
2024-08-01 08:08:29 +0200

    Update Parameters_1.py

--- Parameters_1.py
+++ Parameters_1.py
@@ -23,6 +23,7 @@ v = np.zeros((2**M, 2**M), dtype=complex)
 vv = np.zeros((2**M, 2**M), dtype=complex)
 A = np.zeros((it+1, 2**M*2**M), dtype=complex)
 B = np.zeros((it+1), dtype=complex)
+xaxis = np.zeros((it+1))
 
 # Initialize w
 w = np.zeros((2**M))
@@ -57,6 +58,7 @@ f = np.random.uniform(1, 1, (4, 4)) #This is the input: a 4x4 matrix
 # Perform the optimization loops
 for yy in range (it+1):
    f[1,1] = -4 +yy/80
+   xaxis[yy] = f[1,1]
    Aux = AllPauli * f[:, :, np.newaxis, np.newaxis]
    Ham = sum(Aux[i,j] for i in range (4) for j in range(4))
    def fun_to_minimize(params1): 
@@ -82,46 +84,25 @@ print("Ansatz paramaters =", np.reshape(A[0].real,(4,4))) #These are the output 
 
 A = A.real
 # Plot the results
-plt.plot(Ene[:, 0].real, label='Ene 1')
-plt.plot(Ene[:, 1].real, label='Ene 2')
-plt.plot(Ene[:, 2].real, label='Ene 3')
-plt.plot(Ene[:, 3].real, label='Ene 4')
+plt.plot(xaxis,Ene[:, 0].real, label='Ene 1')
+plt.plot(xaxis,Ene[:, 1].real, label='Ene 2')
+plt.plot(xaxis,Ene[:, 2].real, label='Ene 3')
+plt.plot(xaxis,Ene[:, 3].real, label='Ene 4')
 
 plt.legend()
-plt.xlabel('Iteration')
+plt.xlabel('A_11')
 plt.ylabel('Energy')
 plt.show()
 
-
-plt.plot(A[:, 0].real, label='A 11')
-plt.plot(A[:, 1].real, label='A 12')
-plt.plot(A[:, 2].real, label='A 13')
-plt.plot(A[:, 3].real, label='A 14')
-plt.plot(A[:, 4].real, label='A 21')
-plt.plot(A[:, 5].real, label='A 22')
-plt.plot(A[:, 6].real, label='A 23')
-plt.plot(A[:, 7].real, label='A 24')
-plt.plot(A[:, 8].real, label='A 31')
-plt.plot(A[:, 9].real, label='A 32')
-plt.plot(A[:, 10].real, label='A 33')
-plt.plot(A[:, 11].real, label='A 34')
-plt.plot(A[:, 12].real, label='A 41')
-plt.plot(A[:, 13].real, label='A 42')
-plt.plot(A[:, 14].real, label='A 43')
-plt.plot(A[:, 15].real, label='A 44')
-
-
-  
-plt.xlabel('Iteration')
+for j in range(16):
+   plt.plot(xaxis,A[:, j].real, label='A_ij')
+plt.xlabel('A_11')
 plt.ylabel('Ansatz param')
 plt.show()
 
 plt.plot(B[:].real, label='HS')
-
-
-  
-plt.xlabel('Iteration')
-plt.ylabel('Ansatz param')
+plt.xlabel('A_11')
+plt.ylabel('Frobenius norm of A')
 plt.show()
 
 
