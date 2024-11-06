5a45717d8517713cc0595b88fc1efcb6fbacb9e7
weilei at A8 <weileizeng@quanta.org.cn>
2024-11-06 11:17:35 +0800

    clear code

--- model_hubbard.py
+++ model_hubbard.py
@@ -14,8 +14,6 @@ L=5 # to decide data shape and position
 gpu=4  # the indexed gpu to be used
 
 folder='/data/zwl/hubbard'
-#filename=f'{folder}/eigen.npy'
-#filename=f'{folder}/eigen100000.npy'
 #filename=f'{folder}/h10-0.npy'  # L=5
 filename=f'{folder}/L5n2-h10-0.npy'  # L=5， added decoherence data
 #filename=f'{folder}/L8n2-h10-0.npy'  # L=8
@@ -25,7 +23,7 @@ eval_size_max = 100
 
 _=filename.split('/')[-1]  # folder/name.npy -> name
 #tag='-20000-v0.3'
-tag='-2000-v1.3'
+tag='-2000-v1.4'
 #tag='-200-v0.4'
 #tag='-20-v0.8'  #tag='-20-v0.5'
 _=_+tag
@@ -34,9 +32,7 @@ filename_loss=f'results/{_}.loss.32'
 print('input/output files:',filename,filename_checkpoint,filename_loss)
 filename_config_json=f'results/{_}.json'
 
-# config
-#trials=30
-#output_width=95-9
+# Nerual network hyper paramters
 output_width=10
 hidden_size= 64
 num_hidden_layers=3
@@ -47,9 +43,10 @@ LAYERS[-1]=output_width
 learning_rate  = 0.00001  #0.0001
 n_epochs = 11200 #250   # number of epochs to run
 batch_size = 8*1 #10  # size of each batch
+
+
 #torch.set_printoptions(8)
 torch.set_printoptions(linewidth=140)
-
 #torch.set_default_dtype(torch.float64)
 
 # Get cpu, gpu or mps device for training.
@@ -65,65 +62,40 @@ device = (
 # or use the following inside python
 if device == "cuda":
     torch.cuda.set_device(gpu)
-print(f"Using {device} device")
+print(f"Using {device} device with gpu={gpu}")
 
 print('loading data...') 
 #d = np.load('eigen.npy')
-d = np.load(filename)
-d = torch.tensor(d)
+data = np.load(filename)
+data = torch.tensor(data)
 
-
-eval_size=d.shape[0]//5 #use 20% for test
-#eval_size_min = 1000
+# Adjust evaluation dataset size
+eval_size=data.shape[0]//5 #use 20% for test
 if eval_size < eval_size_min:
     evale_size = eval_size_min
 if eval_size > eval_size_max:
     eval_size = eval_size_max
 
+# Adjust dataset size
 #d = d[:20000] # limit data
-d = d[:(truncate_data_size+eval_size)] # limit data
-print(d)
+data = data[:(truncate_data_size+eval_size)] # limit data
+#print(data)
 
 #eval_size_end = eval_size + 1000 # default -1
 
+print('sample data entry data[0]')
+print(data[0])
+data=data.float()  #differ by 1e-9   # float is much faster and usually accurate enough; to use double, comment this line and set default type to be 
 
-#d = torch.load(filename)
-
-#truncation = 1e-15
-
-#d = d * (d.abs()>truncation) 
-
-print('sample entry d[0]')
-print(d[0])
-#d1=d[1]
-d=d.float()  #differ by 1e-9   # to use double, comment this line and set default type to be 
-#d2=d[1]
-#print(d[1])
-
-#print(d1-d2.double())
-X = d[:,:10]
+# Get training data pair and evaluation data
+X = data[:,:10]
 #y = d[:,11:22]
-y = d[:,12:22]
-energy_from_file = d[:,11]
-obsevables_from_file = d[:,-2:]
-#y=y.reshape((len(y),1))
-#X = d['X']
-#y = d['y']
-# X = X[:10000] #achieve same acc using 10000 entries instead of 40000 entries
-# y = y[:10000]
-print('y',y)
+y = data[:,12:22]
+energy_from_file = data[:,11]
+obsevables_from_file = data[:,-2:]
 
 print('data shape X Y',X.shape,y.shape)
 print(type(X),X.dtype)
-#input('...')
-#torch.save(data,filename)
-#X_test,y_test = X[:1000],y[:1000]
-#print('test shape X Y',X_test.shape,y_test.shape)
-
-#exit()
-
-#def verify_data(s_X,s_y,s_energy)
-
 
 from configurator import print_config, save_config
 #additional_keys=['filelist','LAYERS']
@@ -131,10 +103,6 @@ from configurator import print_config, save_config
 config_keys, config = print_config(globals())
 save_config(config, filename_config_json)
 
-
-#def Xy2energy(_X,_y):
-#def get_energy(uu,state):
-#    eigennumH[nn,u] = np.matmul(np.matmul(np.conj(state[u]),Hamil),state[u])             #energy calculation
 
 from data_generator_Hubbard import Xy2energy, Xy2acc
 mse = nn.MSELoss()
@@ -235,13 +203,9 @@ def model_train(model, X_train, y_train, X_val, y_val,best_acc_energy=-np.inf,be
     for i in [X_train, y_train, X_val, y_val]:
         print(i.shape)
     # loss function and optimizer
-    ##loss_fn = nn.BCELoss()  # binary cross entropy
-    #loss_fn = nn.CrossEntropyLoss()
     loss_fn = nn.MSELoss()
-    loss_list=[]
-    
+    loss_list=[]    
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
 
     batch_start = torch.arange(0, len(X_train), batch_size)
 
@@ -250,7 +214,6 @@ def model_train(model, X_train, y_train, X_val, y_val,best_acc_energy=-np.inf,be
     best_acc_energy = acc_energy
     acc_decoherence = -10
     best_acc_decoherence = acc_decoherence
-    #acc_ref = acc_energy
     acc_ref = acc_ref0
 
     for epoch in range(n_epochs):
@@ -264,9 +227,8 @@ def model_train(model, X_train, y_train, X_val, y_val,best_acc_energy=-np.inf,be
                 y_batch = y_train[start:start+batch_size]
                 X_batch,y_batch = X_batch.to(device),y_batch.to(device)
                 #print('X_batch.shape',X_batch.shape)
-                # forward pass
 
-                
+                # forward pass
                 y_pred = model(X_batch)
                 #print(y_pred.shape,y_batch.shape)
                 loss_train = loss_fn(y_pred, y_batch)
@@ -278,9 +240,6 @@ def model_train(model, X_train, y_train, X_val, y_val,best_acc_energy=-np.inf,be
                 # update weights
                 optimizer.step()
                 # print progress                
-                #acc = acc_eval(y_pred,y_batch)
-                #acc = - loss_train
-                #print(acc)
                 bar.set_postfix(
                     loss_ansatz=float(loss_ansatz),
                     best_acc_energy = float(best_acc_energy),
@@ -294,8 +253,6 @@ def model_train(model, X_train, y_train, X_val, y_val,best_acc_energy=-np.inf,be
         X_val=X_val.to(device)
         y_val=y_val.to(device)        
         y_pred = model(X_val)
-                #acc = ((y_pred>0) == y_val).type(torch.float).mean()
-        #acc = acc_eval(y_pred,y_val)
         loss_ansatz = loss_fn(y_pred[:,1:],y_val[:,1:]) # on ansatz
         
         #torch.save(loss_list,filename_loss)
